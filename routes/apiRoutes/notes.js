6664b8667aa10b22106359b50486c756ee7205f3
VSartz <elsartz@gmail.com>
2022-05-03 10:50:10 -0400

    create note

---
+++ routes/apiRoutes/notes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const { notes } = require('../../db/db.json');
+const createNote = require('../../lib/notes');
+
+router.get('/notes', (req, res) => {
+    let saved = notes;
+    res.json(saved);
+    res.send(console.log('GET notes page',JSON.stringify(saved)));
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    let note = createNote(req.body, notes);
+    res.json(note);
+});
+
+module.exports = router;
