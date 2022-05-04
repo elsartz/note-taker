674529d201ee7c1f2d382208602ed961e0c9d786
VSartz <elsartz@gmail.com>
2022-05-04 19:55:43 -0400

    to heroku

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -7,7 +7,7 @@ const createNote = require('../../lib/notes');
 router.get('/notes', (req, res) => {
     let saved = notes;
     return  res.json(saved);
-    // res.send(console.log('GET notes page',JSON.stringify(saved)));
+   
 })
 
 router.post('/notes', (req, res) => {
@@ -17,7 +17,7 @@ router.post('/notes', (req, res) => {
         req.body.id = 0;
     }
     let note = createNote(req.body, notes);
-    res.json(note);
+   return res.json(note);
 });
 
 module.exports = router;
