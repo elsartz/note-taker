a79e96eaa04a5a80ffd43be7b53d6a6094d74894
VSartz <elsartz@gmail.com>
2022-05-07 13:18:49 -0400

    delete note first attemp

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 let  notes  = require('../../db/db.json');
 console.log(notes);
-const createNote = require('../../lib/notes');
+const {createNote, deleteNote} = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     let saved = notes;
@@ -11,6 +11,7 @@ router.get('/notes', (req, res) => {
 })
 
 router.post('/notes', (req, res) => {
+   
     if (notes) {
         req.body.id = notes.length.toString();
     } else {
@@ -19,5 +20,11 @@ router.post('/notes', (req, res) => {
     let note = createNote(req.body, notes);
    return res.json(note);
 });
+
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params;
+    notes = deleteNote(id, notes);
+    res.json(notes);
+})
 
 module.exports = router;
